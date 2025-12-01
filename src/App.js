d9489898a51c7780e253ba465e011dedbc932fa1
Anyeli0412 <anyeli.tamara@utec.edu.pe>
2025-12-01 00:06:12 -0500

    final

--- src/App.js
+++ src/App.js
@@ -6,20 +6,56 @@ import Home from './components/Home';
 import Orders from './components/Orders';
 import OrderDetail from './components/OrderDetail';
 import Profile from './components/Profile';
+import { haySesionActiva } from './utils/sessionUtils';
 import './App.css';
+
+const ProtectedRoute = ({ children }) => {
+  if (!haySesionActiva()) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/orders/:id" element={<OrderDetail />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/orders" 
+            element={
+              <ProtectedRoute>
+                <Orders />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/orders/:id" 
+            element={
+              <ProtectedRoute>
+                <OrderDetail />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/profile" 
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } 
+          />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
     </Router>
